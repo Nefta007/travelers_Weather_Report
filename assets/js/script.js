7659fdf00254ffab7f185727710864e53a0fb63a
Nefta007 <nperc001@ucr.edu>
2023-09-08 16:33:13 -0700

    added script.js and altered the html file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,36 @@
 var apiKey = "f978bf3ec4d9bc9745dd5b39365c8644";
-var currCity;
-var lastSearched = "";
-var submitButton = document.querySelector('#submission');
-var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + currCity + "&appid=" + apiKey;
+var lastSearched = document.getElementById("list-history");
+var submitButton = document.getElementById('user-submission')
+//var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + currCity + "&appid=" + apiKey;
+//var queryUrl = "https:api.openweathermap.org/data/2.5/forecast?q=" + currCity.value + "&appid=" + apiKey;
+const newCity = document.getElementById("user-location");
 
-function pressSubmit(event){
-    event.preventDefault();
-    console.log("hello");
+//submitButton.addEventListener('click', GetUserInfo);
+function GetInfo(){
+    const newName = document.getElementById("user-location");
+    const cityName = document.getElementById("city-name");
+    cityName.innerHTML = "--"+ currCity.value + "--"
+    console.log(cityName);
+
+
+fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + newCity.value + "&appid=f978bf3ec4d9bc9745dd5b39365c8644")
+.then(response=>response.json())
+.then(data=>{
+    for(i=0; i<5; i++){
+        document.getElementById("day" + (i+1) + "Min").innerHTML = "Min:" + Number(data.list[i].main.temp_min -288.53).toFixed(1)+'°';
+    }
+    for(i=0; i<5; i++){
+        document.getElementById("day" + (i+1) + "Max").innerHTML = "Max:" + Number(data.list[i].main.temp_max -288.53).toFixed(1)+'°';
+    }
+    for(i=0; i<5; i++){
+        document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
+    }
+})
+
+.catch(err =>alert("there has been an error"))
 }
+
+// function defaultScreen(){
+//     document.getElementById("user-location").defaultValue = "California";
+//     GetUserInfo();
+// }
