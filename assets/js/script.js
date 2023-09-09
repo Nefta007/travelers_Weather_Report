b0bea033b77a6371ac80b0e8c6dbbbf4243d4469
Nefta007 <nperc001@ucr.edu>
2023-09-09 04:01:07 -0700

    added to html file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,45 @@
+var currCity = document.getElementById("user-location");
+var userButton = document.getElementById("user-submission-btn");
+var userHistory = document.getElementById("listHistory-btn")
 var apiKey = "f978bf3ec4d9bc9745dd5b39365c8644";
-var lastSearched = document.getElementById("list-history");
-var submitButton = document.getElementById('user-submission')
-//var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + currCity + "&appid=" + apiKey;
-//var queryUrl = "https:api.openweathermap.org/data/2.5/forecast?q=" + currCity.value + "&appid=" + apiKey;
-const newCity = document.getElementById("user-location");
+//var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + CityLocation + "&appid=" + apiKey;
 
-//submitButton.addEventListener('click', GetUserInfo);
-function GetInfo(){
-    const newName = document.getElementById("user-location");
-    const cityName = document.getElementById("city-name");
-    cityName.innerHTML = "--"+ currCity.value + "--"
-    console.log(cityName);
-
-
-fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + newCity.value + "&appid=f978bf3ec4d9bc9745dd5b39365c8644")
-.then(response=>response.json())
-.then(data=>{
-    for(i=0; i<5; i++){
-        document.getElementById("day" + (i+1) + "Min").innerHTML = "Min:" + Number(data.list[i].main.temp_min -288.53).toFixed(1)+'°';
+var UserCity = function(event){
+    event.preventDefault();
+    let CityLocation = currCity.value.trim();
+    if(!CityLocation){
+        alert("please input a city");
     }
-    for(i=0; i<5; i++){
-        document.getElementById("day" + (i+1) + "Max").innerHTML = "Max:" + Number(data.list[i].main.temp_max -288.53).toFixed(1)+'°';
+    else{
+        // alert("you chose " + CityLocation);
+        GetWeather(CityLocation)
+        userHistory.value = '';
+        return;
     }
-    for(i=0; i<5; i++){
-        document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
-    }
-})
+};
 
-.catch(err =>alert("there has been an error"))
+var GetWeather = function(CityLocation){
+    var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + CityLocation + "&appid=" + apiKey;
+    fetch(queryUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(CityLocation);
+        for(var i = 0; i<6; i++){
+            document.getElementById('T-' + i).textContent = 'Temp: ' + Number(data.list[i].main.temp).toFixed(1) + '°';
+        }
+        for(var i = 0; i<6; i++){
+            document.getElementById('w-' + i).textContent = 'Wind: ' + Number(data.list[i].wind.speed).toFixed(1) + 'mph';
+        }
+        for(var i = 0; i<6; i++){
+            document.getElementById('h-' + i).textContent = 'Humidity: ' + Number(data.list[i].main.humidity)+ '%';
+        }
+
+        for(var i = 0; i<6; i++){
+            document.getElementById('icon-' + i).src = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
+        }
+
+    })
 }
-
-// function defaultScreen(){
-//     document.getElementById("user-location").defaultValue = "California";
-//     GetUserInfo();
-// }
+userButton.addEventListener("click",UserCity);
