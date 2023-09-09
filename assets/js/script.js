9812f18dd260ef7ea6a4c04ae670082ff5b330f7
Nefta007 <nperc001@ucr.edu>
2023-09-09 06:28:51 -0700

    added dates using javascript, as well as changed the html in order to follow format

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,43 +3,63 @@ var userButton = document.getElementById("user-submission-btn");
 var userHistory = document.getElementById("listHistory-btn")
 var apiKey = "f978bf3ec4d9bc9745dd5b39365c8644";
 //var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + CityLocation + "&appid=" + apiKey;
+var dateArray = [];
 
-var UserCity = function(event){
+var UserCity = function (event) {
     event.preventDefault();
     let CityLocation = currCity.value.trim();
-    if(!CityLocation){
+    if (!CityLocation) {
         alert("please input a city");
     }
-    else{
+    else {
         // alert("you chose " + CityLocation);
         GetWeather(CityLocation)
         userHistory.value = '';
+        const newElement = document.getElementById('cityOfChoice');
+        newElement.innerHTML = currCity.value;
         return;
     }
 };
 
-var GetWeather = function(CityLocation){
+var GetWeather = function (CityLocation) {
     var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + CityLocation + "&appid=" + apiKey;
     fetch(queryUrl)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(CityLocation);
-        for(var i = 0; i<6; i++){
-            document.getElementById('T-' + i).textContent = 'Temp: ' + Number(data.list[i].main.temp).toFixed(1) + '°';
-        }
-        for(var i = 0; i<6; i++){
-            document.getElementById('w-' + i).textContent = 'Wind: ' + Number(data.list[i].wind.speed).toFixed(1) + 'mph';
-        }
-        for(var i = 0; i<6; i++){
-            document.getElementById('h-' + i).textContent = 'Humidity: ' + Number(data.list[i].main.humidity)+ '%';
-        }
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(CityLocation);
+            for (var i = 0; i < 6; i++) {
+                document.getElementById('T-' + i).textContent = 'Temp: ' + Number(data.list[i].main.temp).toFixed(1) + '°';
+            }
+            for (var i = 0; i < 6; i++) {
+                document.getElementById('w-' + i).textContent = 'Wind: ' + Number(data.list[i].wind.speed).toFixed(1) + 'MPH';
+            }
+            for (var i = 0; i < 6; i++) {
+                document.getElementById('h-' + i).textContent = 'Humidity: ' + Number(data.list[i].main.humidity) + '%';
+            }
 
-        for(var i = 0; i<6; i++){
-            document.getElementById('icon-' + i).src = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
-        }
+            for (var i = 0; i < 6; i++) {
+                document.getElementById('icon-' + i).src = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
+            }
 
-    })
+        })
 }
-userButton.addEventListener("click",UserCity);
+
+
+
+
+
+for (var i = 0; i < 7; i++) {
+    var future = new Date(); // get today date
+    future.setDate(future.getDate() + i); // add 7 days
+    var finalDate = ((future.getMonth() + 1) < 10 ? '' : '') + (future.getMonth() + 1) + '-' + future.getDate() + '-' + future.getFullYear();
+    dateArray.push(finalDate);
+    for (var i = 0; i < dateArray.length; i++) {
+        document.getElementById('day-' + i).textContent = dateArray[i];
+    }
+    //console.log(dateArray);
+}
+
+userButton.addEventListener("click", UserCity);
+
